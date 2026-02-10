a658020d62785ed0e68b060734248a28c297ebef
Rhea <napkinorton@gmail.com>
2026-02-10 11:23:28 -0500

    test: use logback-test.xml

--- src/test/java/com/duckblade/osrs/fortis/debugplugins/FortisColosseumDebugPlugin.java
+++ src/test/java/com/duckblade/osrs/fortis/debugplugins/FortisColosseumDebugPlugin.java
@@ -1,14 +1,11 @@
 package com.duckblade.osrs.fortis.debugplugins;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import javax.inject.Singleton;
 import javax.swing.SwingUtilities;
 import net.runelite.api.events.CommandExecuted;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import org.slf4j.LoggerFactory;
 
 @Singleton
 @PluginDescriptor(
@@ -22,16 +19,12 @@ public class FortisColosseumDebugPlugin extends Plugin
 	@Override
 	protected void startUp()
 	{
-		((Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).setLevel(Level.WARN);
-		((Logger) LoggerFactory.getLogger("com.duckblade.osrs.fortis")).setLevel(Level.DEBUG);
-
 		SwingUtilities.invokeLater(() -> debugPanel = injector.getInstance(FortisColosseumDebugPanel.class));
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		((Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).setLevel(Level.DEBUG);
 	}
 
 	@Subscribe
